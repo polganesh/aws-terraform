135342390c153e257745f40038a248e968264a62
Ganesh Pol <polganesh@gmail.com>
2018-08-16 02:57:04 +0530

    adding access policy for esearch example

--- elasticSearch-example/dev/esearch-public.tf
+++ elasticSearch-example/dev/esearch-public.tf
@@ -1,3 +1,7 @@
+data "aws_region" "current"{}
+
+data "aws_caller_identity" "current" {}
+
 module "esearch-public"{
 	source="../../modules/elasticsearch/public"
 	es_version="6.2"
@@ -18,8 +22,22 @@ module "esearch-public"{
 	compliance="none"
 	confidentiality="confidential"
 	build_date="13082018"
-	maintainance_day="sun"
-	maintainance_time="0001"
 	version_id="001"
 	seq_id="1"
+	access_policies= <<CONFIG
+		{
+			"Version": "2012-10-17",
+			"Statement": [
+				{
+					"Action": "es:*",
+					"Principal": "*",
+					"Effect": "Allow",
+					"Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/dev-it-infra-test-001/*",
+					"Condition": {
+						"IpAddress": {"aws:SourceIp": ["88.98.242.199/32"]}
+					}
+				}
+			]
+		}
+	CONFIG
 }

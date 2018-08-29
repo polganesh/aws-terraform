82c171005618a68e4c40627e942a9acd8288eadb
Ganesh Pol <polganesh@gmail.com>
2018-08-30 01:08:03 +0530

    removing .terraform

--- examples/r53-records/r53-records-simple-routing/dev/.terraform/modules/edcf37ae863de2fbbd9f0c60d6980258/main.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_route53_record" "main" {
-	zone_id = "${var.primaryzone_id}"
-	name    = "${var.record_name}"
-	type    = "${var.record_type}"
-	ttl     = "${var.ttl}"
-	records = ["${var.records}"]
-}
